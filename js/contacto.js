f4b8b313cf3036a1fa96458ebbd2ec3f3ef10aad
Jose David Arce González <jarceg884@ulacit.ed.cr>
2023-08-22 21:52:35 -0600

    Ultima subida
    :)

--- js/contacto.js
+++ js/contacto.js
@@ -1,15 +1,79 @@
-// contacto.js
+/*
+    CRUD JS & FIREBASE para Contactos
+    01/08/2023
+*/
 
-document.addEventListener('DOMContentLoaded', function() {
-    let aboutSection = document.querySelector('.container.about');
+"use strict";
 
-    // Anima la opacidad y desplazamiento vertical al cargar la página
-    aboutSection.style.opacity = '0';
-    aboutSection.style.transform = 'translateY(-50px)';
-    aboutSection.style.transition = 'all 0.5s ease-out';
+// Inicialización de Firebase (asegúrate de tener esta parte sólo una vez en todo tu proyecto)
+const db = firebase.firestore();
 
-    setTimeout(() => {
-        aboutSection.style.opacity = '1';
-        aboutSection.style.transform = 'translateY(0)';
-    }, 100);
+// Colección para Contactos
+const coleccionContactos = "Contactos";
+
+// Referencia a elementos del DOM
+const contactForm = document.querySelector("#contactForm");
+const contactTable = document.querySelector("#contactTable>tbody");
+
+/* FUNCIONES CRUD PARA CONTACTOS */
+
+const findAllContacts = () => db.collection(coleccionContactos).get();
+
+const insertContact = (contactData) =>
+  db.collection(coleccionContactos).doc().set(contactData);
+
+const deleteContact = (paramId) => db.collection(coleccionContactos).doc(paramId).delete();
+
+// Obtener todos los contactos y mostrarlos en una tabla
+window.addEventListener("load", async () => {
+    await findAllContacts().then((query) => {
+        contactTable.innerHTML = "";
+        
+        query.forEach((documento) => {
+            let contacto = documento.data();
+            contactTable.innerHTML += `
+                <tr>
+                    <td>${contacto.nombre}</td>
+                    <td>${contacto.email}</td>
+                    <td>${contacto.mensaje}</td>
+                    <td>
+                        <button class="btn btn-danger btn-borrar mx-1" data-id="${documento.id}">Borrar</button>
+                    </td>
+                </tr>
+            `;
+        });
+
+        // Funcionalidad para botones de borrar
+        const btnBorrar = document.querySelectorAll(".btn-borrar");
+        btnBorrar.forEach((btn) => {
+            btn.addEventListener("click", async (ev) => {
+                if (confirm("Desea borrar el registro de contacto?")) {
+                    await deleteContact(ev.target.dataset.id);
+                    Toastify({
+                        text: "Contacto eliminado exitosamente!",
+                        duration: 3000
+                    }).showToast();
+                }
+            });
+        });
+    });
 });
+
+// Evento submit para el formulario de contacto
+contactForm.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    let nombreContacto = contactForm.txtNombreContacto.value;
+    let emailContacto = contactForm.txtEmailContacto.value;
+    let mensajeContacto = contactForm.txtMensajeContacto.value;
+    
+    await insertContact({ nombre: nombreContacto, email: emailContacto, mensaje: mensajeContacto });
+    
+    Toastify({
+        text: "Mensaje de contacto enviado correctamente",
+        duration: 3000
+    }).showToast();
+
+    contactForm.reset();
+});
+
---
+++ js/firebase.config.js
@@ -0,0 +1,13 @@
+//1. CREAR LA CONSTANTE DE CONEXION
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbhE0-Ymfo9yGWe-3qP4WqkSCExfrH-EY",
+  authDomain: "crudsjs.firebaseapp.com",
+  projectId: "crudsjs",
+  storageBucket: "crudsjs.appspot.com",
+  messagingSenderId: "128480756682",
+  appId: "1:128480756682:web:4bdc6dcf0be0153d4cc533"
+};
+
+firebase.initializeApp(firebaseConfig);
